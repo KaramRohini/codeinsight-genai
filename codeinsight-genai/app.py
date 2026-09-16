af71570ff0985472bd9837f318c53d2ca36ef60d
KaramRohini <karamrohini18@gmail.com>
2026-09-16 16:18:03 +0530

    Update app.py

--- codeinsight-genai/app.py
+++ codeinsight-genai/app.py
@@ -34,7 +34,7 @@ task = st.sidebar.selectbox(
 )
 
 model_choice = st.sidebar.selectbox(
-    "Select Model", ["gemini-1.5-flash", "gemini-1.5-pro"]
+    "Select Model", ["gemini-2.5-flash", "gemini-2.0-flash","gemini-1.5-flash"]
 )
 
 # Main Interface Layout
